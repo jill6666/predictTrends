faf351c703de3c378835fb6efe508364a9625824
jillchang <jill.chang@jkos.com>
2022-12-12 20:41:10 +0800

    feat: automating counter

--- packages/hardhat/contracts/SelfAutomatingCounter.sol
+++ packages/hardhat/contracts/SelfAutomatingCounter.sol
@@ -2,8 +2,8 @@
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
-import {AutomationRegistryInterface, State, Config} from "@chainlink/contracts/src/v0.8/interfaces/AutomationRegistryInterface1_2.sol";
-import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import { AutomationRegistryInterface, State, Config } from "@chainlink/contracts/src/v0.8/interfaces/AutomationRegistryInterface1_2.sol";
+import { LinkTokenInterface } from "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
 
 interface KeeperRegistrarInterface {
     function register(
@@ -44,7 +44,7 @@ contract SelfAutomatingCounter {
     AutomationRegistryInterface public immutable i_registry;
     bytes4 registerSig = KeeperRegistrarInterface.register.selector;
 
-    // pass the token and the registry info
+    // Pass the token and the registry info
     constructor(
         LinkTokenInterface _link,
         address _registrar,
@@ -67,5 +67,74 @@ contract SelfAutomatingCounter {
         counterToLastTimeStamp[counterID] = block.timestamp;
         
         return counterID;
+    }
+
+    function registerAndPredictID(
+        string memory name,
+        uint32 gasLimit,
+        uint96 amount
+    ) public {
+        // Create a new counter
+        uint256 counterID = createNewCounter();
+        (State memory state, Config memory _c, address[] memory _k) = i_registry
+            .getState();
+        uint256 oldNonce = state.nonce;
+        // Pass into counter ID as check data
+        bytes memory checkData = abi.encodePacked(counterID);
+        // Encode the data to send registrar
+        bytes memory payload = abi.encode(
+            name,
+            '0x',
+            address(this),
+            // 999999
+            gasLimit,
+            address(msg.sender),
+            checkData,
+            // 5000000000000000000
+            amount,
+            0,
+            address(this)
+        );
+        // transfer LINK token to registrar
+        i_link.transferAndCall(registrar, amount, bytes.concat(registerSig, payload));
+        (state, _c, _k) = i_registry.getState();
+
+        uint256 newNonce = state.nonce;
+        if(newNonce == oldNonce + 1) {
+            uint256 upkeepID = uint256(keccak256(
+                abi.encodePacked(
+                    blockhash(block.number - 1),
+                    address(i_registry),
+                    uint256(oldNonce)
+                )
+            ));
+            // Set the upkeep id for the counter id
+            counterToUpKeepID[counterID] = upkeepID;
+        } else {
+            revert('auto-approve disabled');
+        }
+    }
+
+    // checkUpkeep expects the counter id as the check data this will run for each new counter
+    function checkUpkeep(bytes calldata checkData) external view returns (bool upkeepNeeded, bytes memory performData) {
+        // Decode the check data
+        uint256 counterID = abi.decode(checkData, (uint256));
+        // If the interval has passed then return true
+        upkeepNeeded = (block.timestamp - counterToLastTimeStamp[counterID]) > interval;
+        // Pass checkData through
+        performData = checkData;
+    }
+
+    function performUpkeep(bytes calldata performData) external {
+        // Decode the check data
+        uint256 counterID = abi.decode(performData, (uint256));
+
+        // revalidating the upkeep in the performUpkeep function
+        if ((block.timestamp - counterToLastTimeStamp[counterID]) > interval) {
+            // Update the last update time
+            counterToLastTimeStamp[counterID] = block.timestamp;
+            // Increment the value
+            counterToValue[counterID] = counterToValue[counterID] + 1;
+        }
     }
 }
