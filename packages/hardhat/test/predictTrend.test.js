0efb91d3c9a3b012597484bcf64d355c9cfefe87
jillchang <jill.chang@jkos.com>
2022-12-13 01:48:48 +0800

    feat: test case

---
+++ packages/hardhat/test/predictTrend.test.js
@@ -0,0 +1,15 @@
+describe("My Dapp", function () {
+  /**
+   * - round should be started automatically by chainlilnk at 20:00 in every day
+   * - round info should be setup only by admin
+   *    - shotPrice
+   *    - refundFee
+   *    - claimFee
+   * - order should be created by user
+   * - order should be updated by user
+   * - order should be refunded by user
+   * - round should be locked automatically by chainlilnk at 16:00 in every day
+   * - executed should be triggered automatically by chainlilnk at 16:00 in every day
+   * - admin should withdraw value from the contract
+   */
+});
